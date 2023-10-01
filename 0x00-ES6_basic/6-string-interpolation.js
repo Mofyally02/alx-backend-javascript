146096dc686bda3ff17198bbcda6b1cf2617734b
Mofyally02 <mofyally02@gmail.com>
2023-10-01 15:23:04 +0300

    Update

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,16 +1,12 @@
-import getBudgetObject from './7-getBudgetObject';
-
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
   };
 
-  return fullBudget;
+  return `As of ${year}, it was the seventh-highest income county in the United States\
+, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
+proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
